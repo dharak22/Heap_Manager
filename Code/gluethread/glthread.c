8644885dab6176962929b0edd00179c0c1d04806
dharak22 <solanki.deepak22b@iiitg.ac.in>
2024-10-09 19:28:19 +0530

    add func glthread_add_before/remove_glthread/delete_glthread_list

--- Code/gluethread/glthread.c
+++ Code/gluethread/glthread.c
@@ -22,3 +22,58 @@ void glthread_add_next( glthread_t* curr_glthread , glthread_t* new_glthread )
     new_glthread->right = temp ;
     temp->left = new_glthread ;
 }
+
+void glthread_add_before( glthread_t *curr_glthread , glthread_t * new_glthread )
+{
+    /*Add on top */
+    if( ! curr_glthread->left )
+    {
+        new_glthread->left = NULL ;
+        new_glthread->right = curr_glthread ;
+        curr_glthread->left = new_glthread ;
+        return ; 
+    }
+
+    glthread_t * temp = curr_glthread->left ;
+    temp->right = new_glthread ;
+    new_glthread->left = temp ;
+    new_glthread->right = curr_glthread ;
+    curr_glthread->left = new_glthread ;
+}
+
+void remove_glthread( glthread_t* curr_glthread )
+{
+    if (!curr_glthread->left)
+    {
+        if(curr_glthread->right)
+        {
+            curr_glthread->right->left = NULL ;
+            curr_glthread->right = 0 ;
+            return ;
+        }
+        return ;
+    }
+
+    if ( !curr_glthread->right)
+    {
+        curr_glthread->left->right = NULL ;
+        curr_glthread->left = NULL ;
+        return ;
+    }
+
+    curr_glthread->left->right = curr_glthread->right ;
+    curr_glthread->right->left = curr_glthread->left ;
+    curr_glthread->left = 0 ;
+    curr_glthread->right = 0 ;
+    
+}
+
+void delete_glthread_list( glthread_t* base_glthread )
+{
+    glthread_t* glthreadptr = NULL ;
+
+    ITERATE_GLTHREAD_BEGIN( base_glthread , glthreadptr )
+    {
+        remove_glthread(glthreadptr) ;
+    } ITERATE_GLTHREAD_END(base_glthread , glthreadptr ) ;
+}
