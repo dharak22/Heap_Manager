0c3a9c7066e712e9f2387e15f679d55f4b594d58
dharak22 <solanki.deepak22b@iiitg.ac.in>
2024-10-09 17:45:07 +0530

    add macros IS_GLTHREAD_LIST_EMPTY GLTHREAD_TO_STRUCT BASE ITERATE_GLTHREAD_BEGIN ITERATE_GLTHREAD_END GLTHREAD_GET_USER_DATA_FROM_OFFSET

--- Code/gluethread/glthread.h
+++ Code/gluethread/glthread.h
@@ -17,5 +17,30 @@ void init_glthread ( glthread_t * glthread );
 
 void glthread_add_last ( glthread_t* base_glthread , glthread_t* new_glthread );
 
+#define IS_GLTHREAD_LIST_EMPTY( glthreadptr )   \
+        ( (glthreadptr)->right == 0 && (glthreadptr)->left == 0 )
+
+#define GLTHREAD_TO_STRUCT(fn_name , structure_name , field_name , glthreadptr )   \
+        static inline structure_name * fn_name(glthread_t* glthreadptr ){    \
+            return (structure_name *)((char *)(glthreadptr) - (char*)&(((structure_name *)0)->field_name));  \
+        }
+/* delete safe loop*/
+/*Normal continue and break can be used with this loop macro */
+
+#define BASE(glthreadptr) ((glthreadptr)->right)
+
+#define ITERATE_GLTHREAD_BEGIN( glthreadptrstart , glthreadptr )   \
+        {     \
+            glthread_t * _glthread_ptr = NULL ;    \
+            glthreadptr = BASE(glthreadptrstart);   \
+            for ( ; glthreadptr != NULL ; glthreadptr = _glthread_ptr ){   \
+                _glthread_ptr = (glthreadptr)->right ;
+
+#define ITERATE_GLTHREAD_END( glthreadptrstart , glthreadptr )   \
+            }}
+
+#define GLTHREAD_GET_USER_DATA_FROM_OFFSET(glthreadptr, offset )    \
+        ( void *)((char*)(glthreadptr) - offset )
+
 
 #endif
