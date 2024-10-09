90892047a01806d5054e5feb6404c5ecbadc94a4
dharak22 <solanki.deepak22b@iiitg.ac.in>
2024-10-09 18:28:21 +0530

    add func init_glthread/glthread_add_next

---
+++ Code/gluethread/glthread.c
@@ -0,0 +1,24 @@
+#include "glthread.h"
+#include<stdlib.h>
+
+void init_glthread( glthread_t * glthread )
+{
+    glthread->left = NULL ;
+    glthread->right = NULL ;
+}
+
+void glthread_add_next( glthread_t* curr_glthread , glthread_t* new_glthread )
+{
+    if (! curr_glthread->right)
+    {
+        curr_glthread->right = new_glthread ;
+        new_glthread->left = curr_glthread ;
+        return ;
+    }
+
+    glthread_t * temp = curr_glthread->right ;
+    curr_glthread->right = new_glthread ;
+    new_glthread->left = curr_glthread ;
+    new_glthread->right = temp ;
+    temp->left = new_glthread ;
+}
