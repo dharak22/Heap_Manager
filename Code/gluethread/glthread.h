e9aa6c5d6cf9d61ee1a4d8ad48227f8169560ef5
dharak22 <solanki.deepak22b@iiitg.ac.in>
2024-10-09 17:20:05 +0530

    give functions definition glthread_add_next glthread_add_before emove_glthread init_glthreadglthread_add_last

--- Code/gluethread/glthread.h
+++ Code/gluethread/glthread.h
@@ -7,4 +7,15 @@ typedef struct _glthread
 	struct glthread* right ;
 } glthread_t ;
 
+void glthread_add_next ( glthread_t* base_glthread , glthread_t* new_glthread );
+
+void glthread_add_before ( glthread_t* base_glthread , glthread_t* new_glthread );
+
+void remove_glthread ( glthread_t* glthread );
+
+void init_glthread ( glthread_t * glthread );
+
+void glthread_add_last ( glthread_t* base_glthread , glthread_t* new_glthread );
+
+
 #endif
