97becbcfe72542cd384192a12d0fb4da07ba1bcd
dharak22 <solanki.deepak22b@iiitg.ac.in>
2024-10-10 20:13:31 +0530

    update uapi_mm.h & testapp.c

--- Code/uapi_mm.h
+++ Code/uapi_mm.h
@@ -2,6 +2,12 @@
 #define __UAPI_MM__
 
 #include<stdint.h>
+
+void* xcalloc(char* struct_name , int units);
+
+#define XCALLOC(units , struct_name ) \
+	(xcalloc(#struct_name , units))
+
 // initialization functions
 void mm_init();
 
@@ -9,7 +15,9 @@ void mm_init();
 void mm_instantiate_new_page_family
 (char* struct_name,uint32_t struct_size);
 
+void mm_print_memory_usage(char* struct_name);
 void mm_print_registered_page_families();
+void mm_print_block_usage() ;
 
 #define MM_REG_STRUCT(struct_name) \
 	(mm_instantiate_new_page_family(#struct_name, sizeof(struct_name)))
